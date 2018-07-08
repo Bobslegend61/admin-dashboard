6050196e93471dd603158ec5f8de7a4632c3b0bf
Alabi Emmanuel <bobslegend795@gmail.com>
2018-07-08 08:44:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple admin dashboard using react and redux
---
+++ routes/routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const _ = require("lodash");
+
+const router = express.Router();
+
+let data = [
+    { id: 1, username: "Alabi Emmanuel", password: "alabs" },
+    { id: 2, username: "Brad Traversy", password: "brad" },
+    { id: 3, username: "Jane Doe", password: "Jane1234" },
+    { id: 4, username: "Fedrick Lamar", password: "peace" },
+    { id: 5, username: "Wrights Philips", password: "cool" }
+];
+
+router.post("/login", ({ body: { username, password } }, res) => {
+    if(_.find(data, { username, password }) ) {
+        return res.json({ success: true, data });
+    }else {
+        return res.json({ success: true, msg: "No user found" });
+    }
+});
+
+router.delete("/:id", ({ params: { id } }, res) => {
+    let deleted = _.find(data, { id: Number(id) });
+    return res.json(deleted);
+});
+
+router.put("/edit/:id", ({ params: { id }, body: { username, password } }, res) => {
+   data.find(user => {
+       if(Number(id) === user.id) {
+           user.username = username;
+           user.password = password
+       }
+   });
+   return res.json({ success: true, data });
+});
+
+// Export router
+module.exports = router;
